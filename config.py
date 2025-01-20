3d5395572c7dece4672c3216a250cea3078cdc31
HerokuTaccc <154675149+HerokuTaccc@users.noreply.github.com>
2025-01-20 21:37:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
 
 # Chat id of a group for logging bot's activities
 LOGGER_ID = int(getenv("LOGGER_ID", None))
-
+MAXRAM = int(getenv("MAXRAM", 1000000))
 # Get this value from @FallenxBot on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", 1356469075))
 
