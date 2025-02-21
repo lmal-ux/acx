b7ebfba652f452fb6229d59ba708205f206a7355
lmal-ux <kuzunohalit@gmail.com>
2025-02-22 00:16:24 +0530

    Update cookieHandler.py

--- cookies/cookieHandler.py
+++ cookies/cookieHandler.py
@@ -1,24 +1,29 @@
 import os
 import sys
 import requests
-from config import LOGGER_ID
+from config import LOGGER_ID 
 from config import BOT_TOKEN
+from http.cookiejar import MozillaCookieJar as surefir
+areCookiesValid = False
 cookiePath = os.path.join(os.getcwd(), "cookies", "cookies.txt") if os.path.exists(os.path.join(os.getcwd(), "cookies", "cookies.txt")) else (requests.get(f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage", params={"chat_id": LOGGER_ID, "text": "Please set up the cookie file ('cookies/cookies.txt')."}).status_code == 200 or True) and sys.exit("Please set up the cookie file ('cookies/cookies.txt').")
-cookies = {
-    name: value
-    for line in open(cookiePath)
-    if line.strip() and not line.startswith("#")
-    for parts in [line.strip().split("\t")]
-    if len(parts) >= 7
-    for domain, flag, path, secure, expiration, name, value in [parts]
-}
 
-url = "https://www.youtube.com/feed/subscriptions"
-text = requests.get(url, cookies=cookies).text.lower()
-urlt = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-params = {
-    "chat_id": LOGGER_ID,
-    "text": f"Cookies are not valid. Please update the cookies.\n__**ERROR**__\n",
-    "parse_mode": "Markdown"
-}
-areCookiesValid = True if "\"logged_in\":true" in text else ((requests.get(urlt, params).status_code == 200) or (print("Failed to send notification: Cookies Aren't Valid") or True)) and sys.exit("Script stopped.")
+def loadCookie(cookiePath=cookiePath):
+    cookies = surefir(cookiePath)
+    cookies.load(ignore_discard=True, ignore_expires=True)
+    return cookies
+
+
+cookies=loadCookie()
+
+
+def checkCookie(cookies=cookies):
+    global areCookiesValid
+
+    url = "https://www.youtube.com/feed/subscriptions"
+    
+    response = requests.get(url, cookies=cookies)
+    
+    text = response.text.lower()
+
+    if "\"logged_in\":true" in text:
+        areCookiesValid = True
