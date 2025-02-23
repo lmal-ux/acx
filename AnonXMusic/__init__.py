6cae881c340f5420646aabe52067d2361294d9f4
HerokuTaccc <154675149+HerokuTaccc@users.noreply.github.com>
2025-02-23 18:27:40 +0530

    Update __init__.py

--- AnonXMusic/__init__.py
+++ AnonXMusic/__init__.py
@@ -1,13 +1,12 @@
+# from AnonXMusic.core.git import git
 from AnonXMusic.core.bot import Anony
 from AnonXMusic.core.dir import dirr
-from AnonXMusic.core.git import git
 from AnonXMusic.core.userbot import Userbot
 from AnonXMusic.misc import dbb, heroku
 
 from .logging import LOGGER
 
 dirr()
-git()
 dbb()
 heroku()
 
